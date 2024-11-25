db935eacf3e1d8b23b47bf18e0c8aac229115a8c
saki0327 <r202401738wr@jindai.jp>
2024-11-25 23:52:17 +0900

    20241125

---
+++ de12/xbp3/main.py
@@ -0,0 +1,17 @@
+from utils_text_mining import text_mining
+import importlib
+importlib.reload(text_mining)
+import warnings
+warnings.filterwarnings('ignore', category=FutureWarning)  # FutureWarningを無視
+
+# 除外する単語の指定
+stopwords = ["する", "ある", "あと", "いる", "いう", "なる", "思う", "言う", "できる","さん","者","日","関西","市","年","会場","会","人","府","来年","ない","円","計画","これ","それ","さ","よる","的"]
+#除外する頻出上位の指定
+stop_n_top = 5
+# 除外する頻出回数以下の指定
+stop_min_freq = 3
+# 除外するノードのエッジ数の指定
+min_edge_frequency = 3
+
+text_mining.create(stopwords, stop_n_top, stop_min_freq, min_edge_frequency)
+
